f4b16d5374b22d2bd316577c0ef9741d546df91d
epic.chen <epicer.chen@gmail.com>
2024-03-28 11:17:32 +0800

    加入專案檔案。

---
+++ WindowsCredentialsConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using CredentialManagement;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("請輸入 Target ID (網際網路或網路位置): ");
+       
+        var targetId = Console.ReadLine();
+
+        Console.WriteLine("-----------------------");
+
+        //讀取帳號密碼
+        var storedCred = new Credential { Target = targetId };
+        if (storedCred.Load())
+        {
+            Console.WriteLine($"Username = {storedCred.Username}");
+            Console.WriteLine($"Password = {storedCred.Password}");
+        }
+
+        // 參考: https://blog.darkthread.net/blog/windows-credentials-management/
+    }
+}
